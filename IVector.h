c3bf5810c1c5246f0706cb18968787043224995b
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-03-07 13:28:09 +0300

    added: - logger interface - set interface - result code enum update: - vector interface

--- IVector.h
+++ IVector.h
@@ -1,21 +1,35 @@
 #ifndef IVECTOR_H
 #define IVECTOR_H
 
+#include<stddef.h>
+#include "ILogger.h"
 
-class IVector{
+
+
+class IVector {
 public:
-    static IVector* createVector(size_t dim, double* pData);
-    virtual IVector* clone() const = 0;
-    static IVector* add(IVector const* pOperand1, IVector const* pOperand2);
-    static IVector* sub(IVector const* pOperand1, IVector const* pOperand2);
-    static IVector* mul(IVector const* pOperand1, double scaleParam);
-    static double mul(IVector const* pOperand1, IVector const* pOperand2);
-    virtual double getCoord(size_t index)const = 0;
-    virtual int setCoord(size_t index, double value) = 0;
-    virtual size_t getDim()const = 0;
+    enum class NORM {
+        NORM_1,
+        NORM_2,
+        NORM_INF
+    };
+    static IVector* createVector(size_t dim, double* pData, ILogger* pLogger);
+    virtual ~IVector() = 0;
+    virtual IVector* clone(ILogger* pLogger) const = 0;
+    static IVector* add(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger);
+    static IVector* sub(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger);
+    static IVector* mul(IVector const* pOperand1, double scaleParam, ILogger* pLogger);
+    static double mul(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger);
+    static bool equals(IVector const* pOperand1, IVector const* pOperand2, NORM norm, double tolerance);
+    virtual double getCoord(size_t index, ILogger* pLogger)const = 0;
+    virtual int setCoord(size_t index, double value, ILogger* pLogger) = 0;
+    static double norm(IVector const* pVector, NORM norm);
+    virtual size_t getDim() const = 0;
 protected:
     IVector() = default;
-    virtual ~IVector() = default;
+private:
+    IVector(IVector const& vector) = delete;
+    IVector& operator=(IVector const& vector) = delete;
 };
 
 
---
+++ RC.h
@@ -0,0 +1,7 @@
+#ifndef RC_H
+#define RC_H
+enum class ERR_CODES {
+    _RC_SUCCESS
+};
+
+#endif //RC_H
