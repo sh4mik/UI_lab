8e8e86b7628b8972a0f69a479e4045e636c527d3
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-03-07 13:28:14 +0300

    added: - logger interface - set interface - result code enum update: - vector interface

--- ISet.h
+++ ISet.h
@@ -6,17 +6,17 @@ class ISet {
 public:
 	static ISet* createSet(ILogger* pLogger);
 	virtual~ISet() = 0;
-	virtual int insert(const IVector* pVector) = 0;
-	virtual int get(IVector*& pVector, size_t index)const = 0;
-	virtual int get(IVector*& pVector, IVector const* pSample, IVector::NORM norm, double tolerance) = 0;
+	virtual ERR_CODES insert(const IVector* pVector) = 0;
+	virtual ERR_CODES get(IVector*& pVector, size_t index)const = 0;
+	virtual ERR_CODES get(IVector*& pVector, IVector const* pSample, IVector::NORM norm, double tolerance) = 0;
 	virtual size_t getDim() const = 0; //space dimension
 	virtual size_t getSize() const = 0; //num elements in set
 	virtual void clear() = 0; // delete all
-	virtual int erase(size_t index) = 0; 
-	virtual int erase(IVector const* pSample, IVector::NORM norm, double tolerance) = 0;
+	virtual ERR_CODES erase(size_t index) = 0;
+	virtual ERR_CODES erase(IVector const* pSample, IVector::NORM norm, double tolerance) = 0;
 	virtual ISet* clone()const = 0;
-	static ISet* add(ISet const* pOperand1, ISet const* pOperand2);
-	static ISet* intersect(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm, double tolerance);
+	static ISet* add(ISet const* pOperand1, ISet const* pOperand2, ILogger* pLogger);
+	static ISet* intersect(ISet const* pOperand1, ISet const* pOperand2, IVector::NORM norm, double tolerance, ILogger* pLogger);
 protected:
 	ISet() = default;
 private:
--- IVector.h
+++ IVector.h
@@ -15,15 +15,15 @@ public:
     };
     static IVector* createVector(size_t dim, double* pData, ILogger* pLogger);
     virtual ~IVector() = 0;
-    virtual IVector* clone(ILogger* pLogger) const = 0;
+    virtual IVector* clone() const = 0;
     static IVector* add(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger);
     static IVector* sub(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger);
     static IVector* mul(IVector const* pOperand1, double scaleParam, ILogger* pLogger);
     static double mul(IVector const* pOperand1, IVector const* pOperand2, ILogger* pLogger);
-    static bool equals(IVector const* pOperand1, IVector const* pOperand2, NORM norm, double tolerance);
-    virtual double getCoord(size_t index, ILogger* pLogger)const = 0;
-    virtual int setCoord(size_t index, double value, ILogger* pLogger) = 0;
-    static double norm(IVector const* pVector, NORM norm);
+    static ERR_CODES equals(IVector const* pOperand1, IVector const* pOperand2, NORM norm, double tolerance, bool* result, ILogger* pLogger);
+    virtual double getCoord(size_t index)const = 0;
+    virtual ERR_CODES setCoord(size_t index, double value) = 0;
+    static double norm(IVector const* pVector, NORM norm, ILogger* pLogger);
     virtual size_t getDim() const = 0;
 protected:
     IVector() = default;
