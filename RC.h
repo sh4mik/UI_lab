3b1fcf0ebaea6ece9686de0f9dda526906ab71f4
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-03-11 14:29:31 +0300

    Edit name of enum , add new rc

--- RC.h
+++ RC.h
@@ -1,7 +1,13 @@
 #ifndef RC_H
 #define RC_H
-enum class ERR_CODES {
-    _RC_SUCCESS
+enum class RESULT_CODE {
+    SUCCESS,
+    OUT_OF_MEMORY,
+    BAD_REFERENCE,
+    WRONG_DIM,
+    DIVISION_BY_ZERO,
+    NAN_VALUE
+    
 };
 
 #endif //RC_H
