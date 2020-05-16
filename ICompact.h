863b0ab273d6cd0192122098e892bdc2799b8f15
Elisey Vasilevskiy <48808317+re1nex@users.noreply.github.com>
2020-05-16 13:34:03 +0300

    change icompact by groups

--- ICompact.h
+++ ICompact.h
@@ -9,53 +9,47 @@ class ICompact
 {
 public:
     class iterator;
-    enum InterfaceTypes
-    {
-        INTERFACE_0,
-        DIMENSION_INTERFACE_IMPL
-    };
-
-    virtual int getId() const = 0;
 
     /*factories*/
 
     static ICompact* createCompact(IVector const* const begin, IVector const* const end,IVector const*const step, ILogger*logger);
 
-    /*operations*/
-    virtual RESULT_CODE Intersection(ICompact const& c) = 0;
-    virtual RESULT_CODE Union(ICompact const& c) = 0;
-    virtual RESULT_CODE Difference(ICompact const& c) = 0;
-    virtual RESULT_CODE SymDifference(ICompact const& c) = 0;
-    virtual RESULT_CODE MakeConvex() = 0;
-
     /*static operations*/
-    static ICompact* Intersection(ICompact const* const left, ICompact const* const right, ILogger*logger);
-    static ICompact* Union(ICompact const* const left, ICompact const* const right, ILogger*logger);
-    static ICompact* Difference(ICompact const* const left, ICompact const* const right, ILogger*logger);
-    static ICompact* SymDifference(ICompact const* const left, ICompact const* const right, ILogger*logger);
-    static ICompact* MakeConvex(ICompact const* const left, ICompact const* const right, ILogger*logger);
+    static ICompact* intersection(ICompact const* const left, ICompact const* const right, ILogger*logger);
+    
+    //union
+    static ICompact* add(ICompact const* const left, ICompact const* const right, ILogger*logger);
 
-    /* returns a step with which you can iterate over all domains of compact*/
+    //methods kill closed!!!
+    //static ICompact* Difference(ICompact const* const left, ICompact const* const right, ILogger*logger);
+    //static ICompact* SymDifference(ICompact const* const left, ICompact const* const right, ILogger*logger);
+
+    static ICompact* makeConvex(ICompact const* const left, ICompact const* const right, ILogger*logger);
+
+    /* returns a step, end, begin with which you can iterate over all domains of compact*/
     virtual IVector* getStep() const = 0;
+    virtual IVector* getBegin() const = 0;
+    virtual IVector* getEnd() const = 0;
 
     virtual iterator* end(IVector const* const step = 0) = 0;
     virtual iterator* begin(IVector const* const step = 0) = 0;
 
     virtual RESULT_CODE isContains(IVector const* const vec, bool& result) const = 0;
-    virtual RESULT_CODE isSubSet(ICompact const* const other,bool& result) = 0;
-    virtual RESULT_CODE isSimplyConn(bool& result) = 0;
-    virtual RESULT_CODE isIntersects(ICompact const* const other, bool& result) = 0;
+    virtual RESULT_CODE isSubSet(ICompact const* const other,bool& result) const = 0;
+    virtual RESULT_CODE isIntersects(ICompact const* const other, bool& result) const = 0;
 
     virtual ICompact* clone() const = 0;
 
     /*dtor*/
-    virtual ~ICompact() = default;
+    virtual ~ICompact() = 0;
 
     class iterator
     {
     public:
         //adds step to current value in iterator
         virtual RESULT_CODE doStep() = 0;
+
+        virtual IVector* getPoint() = 0;
 
         //change step
         virtual RESULT_CODE setDirection(IVector const* const dir) = 0;
@@ -74,7 +68,6 @@ public:
     };
 protected:
     ICompact() = default;
-    virtual ~ICompact();
 
 private:
     /*non default copyable*/
